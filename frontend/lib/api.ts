68922f82ca3ecfdf254484a1cce14e05a05a55b3
Solomon <soluwaseun806@gmail.com>
2025-11-16 02:31:51 +0100

    installed supabase

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,5 @@
 export async function fetchBackendHealth(){
-    const response = await fetch ('https://localhost:3001/api/health');
+    const response = await fetch ('http://localhost:3001/api/health');
     const data = await response.json();
     return data;
 }
